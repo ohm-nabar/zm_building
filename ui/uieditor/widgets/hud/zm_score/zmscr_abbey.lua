11f5a328ab5426ba427d1a79eec9e9f481e2cfe3
gamerblast <ohm.nabar@gmail.com>
2026-02-05 18:15:19 -0500

    3 and 4 player score hud fixes, potential pluspoints disappearing fix

--- ui/uieditor/widgets/hud/zm_score/zmscr_abbey.lua
+++ ui/uieditor/widgets/hud/zm_score/zmscr_abbey.lua
@@ -188,7 +188,7 @@ function CoD.ZMScr.new(HudRef, InstanceRef)
     
 	local listing2 = CoD.ZMScr_ListingSm.new(HudRef, InstanceRef)
 	listing2:setLeftRight(true, false, 0.000000, 134.000000)
-	listing2:setTopBottom(true, false, 3, 61)
+	listing2:setTopBottom(true, false, 6, 64)
 	local function listing2Link(arg0)
 		listing2:setModel(arg0, InstanceRef)
 	end
@@ -200,7 +200,7 @@ function CoD.ZMScr.new(HudRef, InstanceRef)
     
 	local listing3 = CoD.ZMScr_ListingSm.new(HudRef, InstanceRef)
 	listing3:setLeftRight(true, false, 0.000000, 134.000000)
-	listing3:setTopBottom(true, false, -42, 16)
+	listing3:setTopBottom(true, false, -29, 29)
 	local function listing3Link(arg0)
 		listing3:setModel(arg0, InstanceRef)
 	end
@@ -212,7 +212,7 @@ function CoD.ZMScr.new(HudRef, InstanceRef)
     
 	local listing4 = CoD.ZMScr_ListingSm.new(HudRef, InstanceRef)
 	listing4:setLeftRight(true, false, 0.000000, 134.000000)
-	listing4:setTopBottom(true, false, -87, -29)
+	listing4:setTopBottom(true, false, -63, -5)
 	local function listing4Link(arg0)
 		listing4:setModel(arg0, InstanceRef)
 	end
